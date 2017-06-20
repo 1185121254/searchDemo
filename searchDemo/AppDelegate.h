86145a883537027c0193b9360e002ce234d9ac76
chaojie <chaojie@chaojiedemac-mini.local>
2017-06-20 15:59:02 +0800

    Initial Commit

---
+++ searchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  searchDemo
+//
+//  Created by chaojie on 2017/6/20.
+//  Copyright © 2017年 chaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
